93cd7ca507aa015dc9dc3f06a3538f7517b624fd
psexetrap <sanartyo@yandex.ru>
2023-07-23 17:00:36 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/lib/supabase/mutation.ts
@@ -0,0 +1,66 @@
+"use server";
+
+import { randomUUID } from "crypto";
+import { supabaseServer } from ".";
+import { revalidatePath } from "next/cache";
+import { db } from "../db";
+import { likes, replies, tweets } from "../db/schema";
+
+export const likeTweet = async ({
+  tweetId,
+  userId,
+}: {
+  tweetId: string;
+  userId: string;
+}) => {
+  await db
+    .insert(likes)
+    .values({
+      tweetId,
+      userId,
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+  revalidatePath("/");
+};
+
+export const unlikeTweet = async ({
+  tweetId,
+  userId,
+}: {
+  tweetId: string;
+  userId: string;
+}) => {
+  const { data, error } = await supabaseServer
+    .from("likes")
+    .delete()
+    .eq("tweet_id", tweetId)
+    .eq("user_id", userId);
+
+  revalidatePath("/");
+  console.log({ data, error });
+};
+
+export const reply = async ({
+  tweetId,
+  userId,
+  replyText,
+}: {
+  tweetId: string;
+  userId: string;
+  replyText: string;
+}) => {
+  // you can verify/check the replyText is truthy
+
+  if (replyText === "") return;
+
+  await db.insert(tweets).values({
+    text: replyText,
+    profileId: userId,
+    isReply: true,
+    replyId: tweetId,
+  });
+
+  revalidatePath(`/tweet/[id]`);
+};
